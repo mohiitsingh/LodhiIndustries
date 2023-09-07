ad913c0d069e9a2c3d4d88876db4b61675e1928e
mohiitsingh <ms6065097@gmail.com>
2023-09-07 18:00:05 +0530

    changes in side

--- script.js
+++ script.js
@@ -19,5 +19,19 @@ function setInt() {
   }, 5000); 
 }
 setInt();
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
 
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
  
